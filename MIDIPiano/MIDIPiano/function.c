e8339d55a672eb961ef250b79ae217ebb5a4b517
Basilion <47267113+KorBasilion@users.noreply.github.com>
2021-11-16 18:53:40 +0900

    feat: Add functions related to MIDI Lib
    function.c 파일 내 midi_get_dev_caps, midi_open, midi_close 기능 추가

--- MIDIPiano/MIDIPiano/function.c
+++ MIDIPiano/MIDIPiano/function.c
@@ -8,7 +8,7 @@ void CALLBACK MIDI_out_proc(HMIDIOUT h_Midi_device, UINT u_msg,
 
 void MIDI_output_error(MMRESULT mm_result)
 {
-	//ø°∑Ø πÆ¿⁄ø≠¿ª ¿˙¿Â«“ πËø≠
+	//ÏóêÎü¨ Î¨∏ÏûêÏó¥ÏùÑ Ï†ÄÏû•Ìï† Î∞∞Ïó¥
 	char sz_err_msg[128];
 
 	midiInGet_error_text(mm_result, (char*)sz_err_msg, sizeof(sz_err_msg));
@@ -29,4 +29,69 @@ long midi_get_dev_id(HMIDIOUT h_MIDI_device)
 	}
 
 	return (long)u_device_id;
+}
+
+void midi_get_dev_caps(HMIDIOUT m_dev_out_handle, MIDIOUTCAPS *caps)
+{
+ 
+    long l_device_id;
+ 
+    MMRESULT result;
+
+    l_device_id=midi_get_dev_id(m_dev_out_handle);
+ 
+    if (l_device_id < 0) return;
+ 
+    result = midi_out_get_dev_caps((UINT)l_device_id, caps, sizeof(MIDIOUTCAPS));
+
+    if(result != MMSYSERR_NOERROR)
+    {
+        midi_output_error(result);
+ 
+    }
+ 
+}
+
+HMIDIOUT midi_open(WORD w_devs_num)
+ 
+{
+    WORD w_max_dev_num=0;
+
+    MMRESULT ui_midi_open=0;
+
+    HMIDIOUT m_dev_handle=NULL;
+
+    w_max_dev_num=midi_in_get_num_devs();
+
+    if (w_devs_num >= w_max_dev_num)
+    {
+        w_devs_num=0;
+    }
+
+    ui_midi_open = midi_out_open(&m_dev_handle, w_devs_num, (DWORD)(midi_out_proc), (DWORD)NULL, CALLBACK_FUNCTION);
+
+    if (ui_midi_open!=MMSYSERR_NOERROR)
+    {
+        midi_output_error(ui_midi_open);
+        return NULL;
+    }
+
+    return m_dev_handle;
+}
+
+LRESULT midi_close(HMIDIOUT m_dev_out_handle)
+{
+    MMRESULT result;
+
+    result = midi_out_close(m_dev_out_handle);
+
+    if(result!=MMSYSERR_NOERROR)
+    {
+        midi_output_error(result);
+        return FALSE;
+    }
+    else {
+        m_dev_out_handle=NULL;
+    }
+    return TRUE;
 }
